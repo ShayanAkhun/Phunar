1f2a35cf031ea51c868731aa1f3adcc63a4e1596
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2025-02-16 00:23:17 +0500

    fixes

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,33 +1,110 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { wixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
+import Image from "next/image";
+import Link from "next/link";
+import DOMPurify from "isomorphic-dompurify";
+import Pagination from "./Pagination";
 
-const ProductList = ({categoryId, limit} : {categoryId:string, limit:number}) => {
-    return (
-        <div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap '>
-            <Link href="/" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
-                <div className='relative w-full h-80'>
-                    <Image className='absolute object-cover rounded-md z-10 hover:opacity-0 transotion-opacity easy duration-500'
-                        src="https://images.pexels.com/photos/30482791/pexels-photo-30482791/free-photo-of-graceful-tricolored-heron-wading-in-calm-waters.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                        alt=''
-                        fill
-                        sizes="28vw"
-                    />
-                    <Image className='absolute object-cover rounded-md'
-                        src="https://images.pexels.com/photos/20259613/pexels-photo-20259613/free-photo-of-model-in-dress-sitting-on-chair.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt=''
-                        fill
-                        sizes="28vw" />
-                </div>
-                <div className='flex justify-between'>
-                    <span className='font-medium' >Product Name</span>
-                    <span className='font-semibold'>$40</span>
-                </div>
-                <div className='text-sm text-gray-500'>My Description</div>
-                <button className='rounded-2xl w-max ring-1 ring-phunar text-phunar py-2 px-4 text-xs hover:bg-phunar hover:text-white  '>Add to Cart</button>
-            </Link>
-        </div>
+const PRODUCT_PER_PAGE = 8;
+
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string;
+  limit?: number;
+  searchParams?: any;
+}) => {
+  const wixClient = await wixClientServer();
+
+  const productQuery = wixClient.products
+    .queryProducts()
+    .startsWith("name", searchParams?.name || "")
+    .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
     );
-}
+  // .find();
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
+
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      {res.items.map((product: products.Product) => (
+        <Link
+          href={"/" + product.slug}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+          key={product._id}
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt=""
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.price?.price}</span>
+          </div>
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
+          )}
+          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+      ))}
+      {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null}
+    </div>
+  );
+};
 
 export default ProductList;
